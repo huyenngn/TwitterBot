05e3901f685cec96a92d5784793d56fb6871dbba
nganhuyenthinguyen <thinguyen@campus.tu-berlin.de>
2023-01-17 22:47:22 +0100

    updated requirements

--- bots/main.py
+++ bots/main.py
@@ -8,9 +8,6 @@ access_token = os.getenv("ACCESS_TOKEN")
 access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
 bearer_token = os.getenv("BEARER_TOKEN")
 
-
-# Authenticate to Twitter
-
 auth = tweepy.OAuth1UserHandler(
    consumer_key, consumer_secret, access_token, access_token_secret
 )
