3815853e811b9c1570554c2e1a98f9dc0c290d8e
Ngan Huyen Thi Nguyen <thinguyen@campus.tu-berlin.de>
2023-01-18 17:27:02 +0100

    init

--- bots/main.py
+++ bots/main.py
@@ -1,6 +1,15 @@
 import tweepy
 import os
+import logging
 from googletrans import Translator
+
+rules = [
+    tweepy.StreamRule('from:srchafreen -is:retweet', tag='freen'),
+    tweepy.StreamRule('from:AngelssBecky -is:retweet', tag='becky')
+]
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
 
 consumer_key = os.getenv("CONSUMER_KEY")
 consumer_secret = os.getenv("CONSUMER_SECRET")
@@ -8,44 +17,40 @@ access_token = os.getenv("ACCESS_TOKEN")
 access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
 bearer_token = os.getenv("BEARER_TOKEN")
 
-auth = tweepy.OAuth1UserHandler(
-   consumer_key, consumer_secret, access_token, access_token_secret
-)
-
-client = tweepy.Client(
+api = tweepy.Client(
     consumer_key=consumer_key,
     consumer_secret=consumer_secret,
     access_token=access_token,
     access_token_secret=access_token_secret
 )
 
-translator = Translator()
-
-rules = [
-    tweepy.StreamRule('from:srchafreen -is:retweet', tag='freen'),
-    tweepy.StreamRule('from:AngelssBecky -is:retweet', tag='becky')
-]
+trans = Translator()
 
 class TranslationAnswer(tweepy.StreamingClient):
+    def on_connect(self): 
+        self.add_rules(rules)
+        logger.info("added rules")
+        response = self.get_rules()
+        for rule in response.data:
+            logger.info(f"{rule}")
 
     def on_tweet(self, tweet):
         result = "[EN] "
-        result += translator.translate(tweet.text, src='th', dst='en').text
-        print(tweet.id, tweet.text, result, tweet.author_id)
-        client.create_tweet(text=result, in_reply_to_tweet_id=tweet.id)
-        client.like(tweet.id)
-        client.retweet(tweet.id)
+        result += trans.translate((tweet.text), src='th', dst='en').text
+        logger.info(f"got tweet... {tweet.id, tweet.text, result, tweet.author_id}")
+        api.create_tweet(text=result, in_reply_to_tweet_id=tweet.id)
+        api.like(tweet.id)
+        api.retweet(tweet.id)
 
     def on_errors(self, errors):
-        print(errors)
+        logger.error(errors)
 
     def on_connection_error(self):
         self.disconnect()
 
+def main():
+    ta = TranslationAnswer(bearer_token=bearer_token, wait_on_rate_limit=True)
+    ta.filter()
 
-ta = TranslationAnswer(bearer_token=bearer_token, wait_on_rate_limit=True)
-ta.add_rules(rules)
-response = ta.get_rules()
-for rule in response.data:
-    print(rule)
-ta.filter()
+if __name__ == "__main__":
+    main()
