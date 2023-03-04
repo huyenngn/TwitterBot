25f08002a540a86a05b91952f06bc6c60c525e0b
Huyen Nguyen <huyenproducts@gmail.com>
2023-03-04 08:31:48 +0100

    wait until network idle

--- bots/modules/thai2eng.py
+++ bots/modules/thai2eng.py
@@ -22,7 +22,7 @@ class Thai2Eng:
         params = urlencode(
             dict(
                 access_key=self.api_flash_key,
-                wait_until="page_loaded",
+                wait_until="network_idle",
                 width="750",
                 full_page="true",
                 js="if (document.querySelector('ul').children.length == 0){document.querySelector('body').setAttribute('style', 'display: none;')} else {document.getElementsByClassName('container')[0].setAttribute('style', 'display: none;');document.querySelector('footer').setAttribute('hidden', 'true');document.querySelector('form').setAttribute('hidden', 'true');};",
